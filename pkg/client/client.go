189e8b49a16be2ad78963c0d286c7c65b452ca51
imHinotori <hello@hinotori.moe>
2023-12-25 19:04:23 -0300

    ✨ Client Structure and Creation

---
+++ pkg/client/client.go
@@ -0,0 +1,23 @@
+package client
+
+import "net/http"
+
+const PlayFlowProductionAPI = "https://api.cloud.playflow.app"
+
+type Client struct {
+	HTTP  *http.Client
+	url   string
+	token string
+}
+
+func NewClient(token string, url ...string) (*Client, error) {
+	if len(url) == 0 {
+		url = append(url, PlayFlowProductionAPI)
+	}
+
+	return &Client{
+		url:   url[0],
+		token: token,
+		HTTP:  http.DefaultClient,
+	}, nil
+}
